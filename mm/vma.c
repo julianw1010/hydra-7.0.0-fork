acc54ae9a18e5d93c3d131ca4cb335b9eff4e418
Julian Waciewski <julian.waciewski@rwth-aachen.de>
2026-06-23 09:37:41 +0200

    hydra: add hydra_fixup_pud_nodes for MAP_FIXED PUD ownership

--- mm/vma.c
+++ mm/vma.c
@@ -607,6 +607,54 @@ static int split_vma(struct vma_iterator *vmi, struct vm_area_struct *vma,
 	return __split_vma(vmi, vma, addr, new_below);
 }
 
+static int hydra_lookup_pud_owner(struct mm_struct *mm,
+				  unsigned long addr,
+				  struct vm_area_struct *exclude)
+{
+	unsigned long pud_start = addr & PUD_MASK;
+	unsigned long pud_end = pud_start + PUD_SIZE;
+	struct vm_area_struct *existing;
+	VMA_ITERATOR(vmi, mm, pud_start);
+
+	for_each_vma_range(vmi, existing, pud_end) {
+		if (existing == exclude)
+			continue;
+		return existing->master_pgd_node;
+	}
+
+	return -1;
+}
+
+void hydra_fixup_pud_nodes(struct mm_struct *mm,
+			   struct vm_area_struct *vma)
+{
+	unsigned long pud_boundary;
+	int node;
+	struct vm_area_struct *cur;
+
+	if (!mm->lazy_repl_enabled)
+		return;
+
+	cur = vma;
+	while (cur) {
+		node = hydra_lookup_pud_owner(mm, cur->vm_start, cur);
+		if (node >= 0)
+			WRITE_ONCE(cur->master_pgd_node, node);
+
+		pud_boundary = (cur->vm_start & PUD_MASK) + PUD_SIZE;
+		if (pud_boundary >= cur->vm_end)
+			break;
+
+		{
+			VMA_ITERATOR(vmi, mm, pud_boundary);
+			if (split_vma(&vmi, cur, pud_boundary, 0))
+				break;
+		}
+
+		cur = find_vma(mm, pud_boundary);
+	}
+}
+
 /*
  * dup_anon_vma() - Helper function to duplicate anon_vma on VMA merge in the
  * instance that the destination VMA has no anon_vma but the source does.
@@ -2784,6 +2832,8 @@ static unsigned long __mmap_region(struct file *file, unsigned long addr,
 		if (error)
 			return error;
 	}
+
+	hydra_fixup_pud_nodes(mm, vma);
 
 	return addr;
 
--- mm/vma.h
+++ mm/vma.h
@@ -267,6 +267,9 @@ static inline pgoff_t vma_pgoff_offset(struct vm_area_struct *vma,
 		.master_pgd_node = vma_->master_pgd_node,	\
 	}
 
+void hydra_fixup_pud_nodes(struct mm_struct *mm,
+			   struct vm_area_struct *vma);
+
 #ifdef CONFIG_DEBUG_VM_MAPLE_TREE
 void validate_mm(struct mm_struct *mm);
 #else
